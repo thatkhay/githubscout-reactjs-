edb5991e5e92a649d35840309de57b3059a4882d
thatkhay <johnkingsleyegeonu@gmail.com>
2023-06-24 00:18:02 +0100

    imported react router and icons. created a nav bar

--- githubfinder/src/App.js
+++ githubfinder/src/App.js
@@ -1,12 +1,21 @@
 
 import './App.css';
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from './components/layout/Navbar';
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-4xl'>Git</h1>
-      <button className='btn'>click</button>
+  
+      <Router>
+    <div className='flex flex-col justify-between h-screen'>
+      <Navbar />
+
+      <main>
+        contrbt
+      </main>
     </div>
+      </Router>
+      
+    
   );
 }
 
