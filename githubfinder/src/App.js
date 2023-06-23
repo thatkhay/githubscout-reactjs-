3118b7a68154a3ae3b50638db152be65cdf47548
thatkhay <johnkingsleyegeonu@gmail.com>
2023-06-23 20:40:22 +0100

    fitst commit

---
+++ githubfinder/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-4xl'>Git</h1>
+      <button className='btn'>click</button>
+    </div>
+  );
+}
+
+export default App;
