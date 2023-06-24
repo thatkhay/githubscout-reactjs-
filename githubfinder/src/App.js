4a7bc3b1d80fc614e3a7392c4faae1ccd6a2600e
thatkhay <johnkingsleyegeonu@gmail.com>
2023-06-24 10:00:30 +0100

    created pages and routes

--- githubfinder/src/App.js
+++ githubfinder/src/App.js
@@ -1,7 +1,12 @@
 
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Notfound from './pages/Notfound';
+
 function App() {
   return (
   
@@ -9,9 +14,15 @@ function App() {
     <div className='flex flex-col justify-between h-screen'>
       <Navbar />
 
-      <main>
-        contrbt
+      <main className='container mx-auto px-3 pb-12'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/notfound' element={<Notfound />} />
+          <Route path='/*' element={<Notfound />} />
+        </Routes>
       </main>
+      <Footer />
     </div>
       </Router>
       
---
+++ githubfinder/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1 className='text-6xl'>welcome</h1>
+    </div>
+  )
+}
+
+export default Home
